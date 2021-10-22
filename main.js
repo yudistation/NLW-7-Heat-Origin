d2ceb1567aa44eee96d9714fe913e3d7c196fbfc
Yudi <gabryudi@gmail.com>
2021-10-21 22:20:00 -0400

    Aula 4

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+    github: 'yuditakeda',
+    youtube: 'pewdiepie',
+    facebook: 'pewdiepie',
+    instagram: 'pewdiepie',
+    twitter: 'pewdiepie'
+}
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            userImg.src = data.avatar_url
+            userLogin.textContent = data.login
+        })
+}
+
+getGitHubProfileInfos()
